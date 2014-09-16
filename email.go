ecc8d6d0a59a2749198c21ef9ceb214884d69bbc
Thomas Sileo <thomas.sileo@gmail.com>
2014-09-16 09:58:44 +0200

    Better email notification

--- email.go
+++ email.go
@@ -8,17 +8,27 @@ import (
 	"github.com/stathat/amzses"
 )
 
+// TODO handle sender email as config
+
+var alertEmailSubjectTpl = `{{.URL}} is {{ if .Up }} up {{ else }} down {{ end }}`
+var alertEmailBodyTpl = `{{.URL}} is {{ if .Up }} up {{ else }} down {{ end }}`
+
 func NotifyEmails(c *Check) error {
 	log.Printf("NotifyEmails %v", c)
-	var buf bytes.Buffer
-	t := template.New("alert mail")
-	template.Must(t.Parse("hello {{.URL}}!"))
-	if err := t.Execute(&buf, c); err != nil {
+	var body, subject bytes.Buffer
+	t := template.New("alert mail body")
+	t2 := template.New("alert mail subject")
+	template.Must(t.Parse(alertEmailBodyTpl))
+	template.Must(t2.Parse(alertEmailSubjectTpl))
+	if err := t.Execute(&body, c); err != nil {
+		panic(err)
+	}
+	if err := t2.Execute(&subject, c); err != nil {
 		panic(err)
 	}
 	for _, email := range c.Emails {
 		log.Printf("Sending mail to %v", email)
-		if _, err := amzses.SendMail("thomas.sileo@gmail.com", email, "Welcome!", buf.String()); err != nil {
+		if _, err := amzses.SendMail("thomas.sileo@gmail.com", email, subject.String(), body.String()); err != nil {
 			return err
 		}
 	}
